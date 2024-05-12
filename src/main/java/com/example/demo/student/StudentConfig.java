d4e06133148851120893a08bfa34c69e3f25bd68
KaraitPL <karaitpl@gmail.com>
2024-05-12 22:00:04 +0200

    Saving to database

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,32 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner comamandLineRunner(StudentRepository repository){
+        return args -> {
+            Student oskar = new Student(
+                    "Oskar",
+                    22,
+                    "owilda@gmail.com",
+                    LocalDate.of(2002, Month.MAY, 11));
+
+            Student pawel = new Student(
+                    "Pawel",
+                    22,
+                    "Pawel@gmail.com",
+                    LocalDate.of(2002, Month.APRIL, 24));
+
+            repository.saveAll(List.of(oskar, pawel));
+        };
+
+    }
+}
